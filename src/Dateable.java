90854a910fe767202a8a9e8b1b1ca543dbd5d7ac
gravo-lan <alvintsolh@gmail.com>
2023-08-31 14:19:02 +0800

    Completed Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.*;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.print("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.println(age + "-year-olds should date somebody who is at least " + (age/2 + 7) + " years old.");
+        scanner.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.*;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        System.out.print("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.print(age + "-year-olds should read at least" + (100-age) + " pages before giving up on a book.");
+        scanner.close();
     }
 }
